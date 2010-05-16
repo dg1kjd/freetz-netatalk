2c39b66624ff0d468bc60221ac7d2807c6b55cd0
Sven Schwyn <schwyn@bitcetera.com>
2010-05-16 18:05:31 +0200

    Fix which lets netatalk compile

--- netatalk.mk
+++ netatalk.mk
@@ -1,9 +1,11 @@
 $(call PKG_INIT_BIN, 2.1)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=@SF/netatalk
-$(PKG)_BINARY:=$($(PKG)_DIR)/afpd
+$(PKG)_BINARY:=$($(PKG)_DIR)/etc/afpd/afpd
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/sbin/afpd
 $(PKG)_SOURCE_MD5:=27d4b8dca55801fe7d194bf6b34852f4
+
+$(PKG)_DEPENDS_ON := db libgcrypt openssl
 
 $(PKG)_CONFIGURE_OPTIONS +=--disable-afs
 $(PKG)_CONFIGURE_OPTIONS +=--enable-hfs
@@ -13,12 +15,13 @@ $(PKG)_CONFIGURE_OPTIONS +=--disable-timelord
 $(PKG)_CONFIGURE_OPTIONS +=--disable-a2boot
 $(PKG)_CONFIGURE_OPTIONS +=--disable-cups
 $(PKG)_CONFIGURE_OPTIONS +=--disable-tcp-wrappers
+$(PKG)_CONFIGURE_OPTIONS +=--disable-admin-group
+$(PKG)_CONFIGURE_OPTIONS +=--disable-srvloc
 $(PKG)_CONFIGURE_OPTIONS +=--with-cnid-default-backend=dbd
+$(PKG)_CONFIGURE_OPTIONS +=--with-bdb="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
 $(PKG)_CONFIGURE_OPTIONS +=--with-includedir="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include"
 $(PKG)_CONFIGURE_OPTIONS +=--with-libdir="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib"
 $(PKG)_CONFIGURE_OPTIONS +=--with-uams-path="/usr/lib/uams"
-$(PKG)_CONFIGURE_OPTIONS +=--disable-admin-group
-$(PKG)_CONFIGURE_OPTIONS +=--disable-srvloc
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -26,7 +29,8 @@ $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(NETATALK_DIR) \
-		$(NETATALK_MAKE_OPTIONS) CPPFLAGS="$(NETATALK_CPPFLAGS)"
+		CPPFLAGS="-I$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include $(NETATALK_CPPFLAGS)" \
+		LDFLAGS="-L$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib"
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
