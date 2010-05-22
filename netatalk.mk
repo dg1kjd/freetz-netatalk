40bbe3242e26bf7bf8972f0ebd2dbf97dd942567
Sven Schwyn <schwyn@bitcetera.com>
2010-05-22 21:59:08 +0200

    Add all binaries, daemons and libs needed

--- netatalk.mk
+++ netatalk.mk
@@ -3,12 +3,19 @@ $(PKG)_SOURCE := $(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5 := 27d4b8dca55801fe7d194bf6b34852f4
 $(PKG)_SITE := @SF/netatalk
 
-$(PKG)_LIBNAMES := uams_guest uams_dhx_passwd
-$(PKG)_LIBS_BUILD_DIR := $(NETATALK_LIBNAMES:%=$($(PKG)_DIR)/etc/uams/.libs/%.so)
-$(PKG)_LIBS_TARGET_DIR := $(NETATALK_LIBNAMES:%=$($(PKG)_DEST_LIBDIR)/%.so)
+$(PKG)_LIBS := uams_guest uams_dhx_passwd
+$(PKG)_LIBS_BUILD_DIR := $(NETATALK_LIBS:%=$($(PKG)_DIR)/etc/uams/.libs/%.so)
+$(PKG)_LIBS_TARGET_DIR := $(NETATALK_LIBS:%=$($(PKG)_DEST_LIBDIR)/%.so)
 
-$(PKG)_BINARY := $($(PKG)_DIR)/etc/afpd/afpd
-$(PKG)_TARGET_BINARY := $($(PKG)_DEST_DIR)/sbin/afpd
+$(PKG)_AFPD_BINS := hash 
+$(PKG)_CNID_BINS := dbd
+$(PKG)_BINS_BUILD_DIR := $(NETATALK_AFPD_BINS:%=$($(PKG)_DIR)/etc/afpd/%) $(NETATALK_CNID_BINS:%=$($(PKG)_DIR)/etc/cnid_dbd/%)
+$(PKG)_BINS_TARGET_DIR := $(NETATALK_AFPD_BINS:%=$($(PKG)_DEST_DIR)/usr/bin/%) $(NETATALK_CNID_BINS:%=$($(PKG)_DEST_DIR)/usr/bin/%)
+
+$(PKG)_AFPD_SBINS := afpd 
+$(PKG)_CNID_SBINS := cnid_metad cnid_dbd
+$(PKG)_SBINS_BUILD_DIR := $(NETATALK_AFPD_SBINS:%=$($(PKG)_DIR)/etc/afpd/%) $(NETATALK_CNID_SBINS:%=$($(PKG)_DIR)/etc/cnid_dbd/%)
+$(PKG)_SBINS_TARGET_DIR := $(NETATALK_AFPD_SBINS:%=$($(PKG)_DEST_DIR)/sbin/%) $(NETATALK_CNID_SBINS:%=$($(PKG)_DEST_DIR)/sbin/%)
 
 $(PKG)_DEPENDS_ON := db libgcrypt openssl
 
@@ -32,7 +39,7 @@ $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
-$($(PKG)_BINARY) $($(PKG)_LIBS_BUILD_DIR): $($(PKG)_DIR)/.configured
+$($(PKG)_LIBS_BUILD_DIR) $($(PKG)_BINS_BUILD_DIR) $($(PKG)_SBINS_BUILD_DIR): $($(PKG)_DIR)/.configured
 	$(SUBMAKE) -C $(NETATALK_DIR) \
 		CPPFLAGS="-I$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include $(NETATALK_CPPFLAGS)" \
 		LDFLAGS="-L$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib"
@@ -40,18 +47,21 @@ $($(PKG)_BINARY) $($(PKG)_LIBS_BUILD_DIR): $($(PKG)_DIR)/.configured
 $($(PKG)_LIBS_TARGET_DIR): $($(PKG)_DEST_LIBDIR)/%: $($(PKG)_DIR)/etc/uams/.libs/%
 	$(INSTALL_LIBRARY_STRIP)
 
-$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+$($(PKG)_BINS_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_BINS_BUILD_DIR)
+	$(INSTALL_BINARY_STRIP)
+
+$($(PKG)_SBINS_TARGET_DIR): $($(PKG)_DEST_DIR)/sbin/%: $($(PKG)_SBINS_BUILD_DIR)
 	$(INSTALL_BINARY_STRIP)
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_LIBS_TARGET_DIR) $($(PKG)_TARGET_BINARY)
+$(pkg)-precompiled: $($(PKG)_LIBS_TARGET_DIR) $($(PKG)_BINS_TARGET_DIR) $($(PKG)_SBINS_TARGET_DIR)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(NETATALK_DIR) clean
 	$(RM) $(NETATALK_FREETZ_CONFIG_FILE)
 
 $(pkg)-uninstall:
-	$(RM) $($(PKG)_LIBS_TARGET_DIR) $(NETATALK_TARGET_BINARY)
+	$(RM) $($(PKG)_LIBS_TARGET_DIR) $($(PKG)_BINS_TARGET_DIR) $($(PKG)_SBINS_TARGET_DIR)
 
 $(PKG_FINISH)
